dbcb7165a9e2d3dcac780683d6b34cfe3ce7dfc9
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2023-02-17 09:35:04 +0200

    Create stepOne.h

---
+++ stepOne.h
@@ -0,0 +1,14 @@
+#ifndef _STEPONE_H
+#define _STEPONE_H
+
+
+#include "declarations.h"
+
+
+/* function declaration */
+
+bool stepOne(line_info line, short* machineCommands, short* machineData, long *ic, long *dc);
+
+
+
+#endif
