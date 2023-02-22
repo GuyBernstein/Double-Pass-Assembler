df8771304e1ef14851387407cb85036dcf0570a3
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2023-02-22 13:17:17 +0200

    Update stepOne.c

--- stepOne.c
+++ stepOne.c
@@ -7,9 +7,13 @@
 #include "stepOne.h"
 #include "utils.h"
 #include "directive.h"
+#include "code.h"
 
 
-bool stepOne(line_info line, short* machineCommands, short* machineData, long *IC, long *dc )
+static bool process_code(line_info line, int i, long *ic, short* machineCommands);
+
+
+bool stepOne(line_info line, short* machineCommands, short* machineData, long *IC, long *dc , table *symbol_table)
 {
 
 	int j;
@@ -35,8 +39,13 @@ bool stepOne(line_info line, short* machineCommands, short* machineData, long *I
 	if (!line.content[i] || line.content[i] == '\n' || line.content[i] == EOF || line.content[i] == ';')
 		return TRUE; /* Empty/Comment line - no errors found (of course) */
 
-		
-		
+	
+	
+	
+	/* if tried to define label, but it's invalid, return that an error occurred. */
+	if (isLabel(line, symbol)) 
+		return FALSE;
+	
 
 
 	if (symbol[0] != '\0') 
@@ -47,9 +56,13 @@ bool stepOne(line_info line, short* machineCommands, short* machineData, long *I
 	MOVE_TO_NOT_WHITE(line.content, i) /* Move to next not-white char */
 	
 	if (line.content[i] == '\n') 
-		return TRUE; /* Label-only line - skip */	
-	
+		return TRUE; /* Label-only line - skip */
+		
 	/* if already defined as data/external/code and not empty line */
+	if (find_by_types(*symbol_table, symbol, 3, EXTERNAL_SYMBOL, DATA_SYMBOL, CODE_SYMBOL)) {
+		printf_line_error(line, "Symbol %s is already defined.", symbol);
+		return FALSE;
+	}	
 	
 	
 	/* Check if it's a directive (starting with '.') */
@@ -64,10 +77,11 @@ bool stepOne(line_info line, short* machineCommands, short* machineData, long *I
 	/* is a directive */
 	if (directive != NONE_DIRE) 
 	{
-			
+		
 		/* if .string or .data, and symbol defined, put it into the symbol table */
-		if((directive == DATA_DIRE || directive == STRING_DIRE) && symbol[0] != '\0');
+		if((directive == DATA_DIRE || directive == STRING_DIRE) && symbol[0] != '\0')
 			/* is data or string, add DC with the symbol to the table as data */
+			add_table_item(symbol_table, symbol, *dc, DATA_SYMBOL);
 			
 		
 		/*found that directive is .string*/
@@ -104,6 +118,7 @@ bool stepOne(line_info line, short* machineCommands, short* machineData, long *I
 				printf_line_error(line, "Invalid external label name: %s", symbol);
 				return TRUE;
 			}
+			add_table_item(symbol_table, symbol, 0, EXTERNAL_SYMBOL); /* Extern value is defaulted to 0 */
 			
 		}
 		
@@ -117,45 +132,47 @@ bool stepOne(line_info line, short* machineCommands, short* machineData, long *I
 	} /* end if (directive != NONE) */
 	
 	/* not a directive=>it's a command! */
-	
-	
-				
-			
-		
+	else 
+	{
+		/* if symbol defined, add it to the table */
+		if (symbol[0] != '\0')
+			add_table_item(symbol_table, symbol, *IC, CODE_SYMBOL);
+		line.content += strlen(symbol);
+		/* Analyze code */
+		return process_code(line, i, IC, machineCommands);
+	}
 	
 	return TRUE;
 }
 
 
+static bool process_code(line_info line, int i, long *ic, short* machineCommands) 
+{
+	char operation[8]; /* stores the string of the current code instruction */
+	char *operands[2]; /* 2 strings, each for operand */
+	opcode curr_opcode; /* the current opcode and funct values */
+	funct curr_funct;
+	long ic_before;
+	int j, operand_count;
+	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
+	/* Skip white chars */
+	MOVE_TO_NOT_WHITE(line.content, i)
+	
+	/* if not too big instruction, copy it: */
+	if(sscanf(line.content,"%6s",operation) == 1)
+		printf("operation %s\n",operation);
+	
+	/* Get opcode & funct by command name into curr_opcode/curr_funct */
+	get_opcode_func(operation, &curr_opcode, &curr_funct);
+	/* If invalid operation (opcode is NONE_OP=-1), print and skip processing the line. */
+	if (curr_opcode == NONE_OP) 
+	{
+		printf_line_error(line, "Unrecognized instruction: %s", operation);
+		return FALSE; /* an error occurred */
+	}
+	return FALSE;
+}
 
 
