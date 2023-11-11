ee06f9ccae244f489dc01d2eb8d4f997b72fc62e
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2023-11-11 09:57:55 +0200

    Update preAssembler.c

--- preAssembler.c
+++ preAssembler.c
@@ -1,114 +1,125 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "preAssembler.h"
 #include "declarations.h"
 
-int skipSpacesAndTabs1(char *s) 
-{
-	int n;
-	for (n = 0; s[n] != 0 && (s[n] == ' ' || s[n] == '\t'); n++)
-		{} /* Count how many spaces there are at the beginning */
-	return n;
-}
 
+/*
+ * This function pre-assembles a given input file that contains macro definitions and macro expansions.
+ * It stores any macro found in the input file into an array of struct macro,
+ * and replaces the macro with its content in the output file.
+ * It does some error checking and frees the memory used by the array of struct macro.
+ */
 bool preAssembler(FILE *ipf, char* fileName) 
 {
 	FILE *opf;
-	char line[MAX_LINE]={'\0'};
-	int curr_macro = 0;
-	int i,replaced;
+	char line[MAX_LINE]={'\0'};/* buffer to hold a line read from the input file*/
+	int curr_macro = 0;/* index of the current macro being processed */
+	int i,replaced;/* temporary variables used in the processing of macros */
 
-    char word[MAX_LINE];
-    struct macro *macros;
-    int lineCount =1;
+    	char word[MAX_LINE];/* buffer to hold a word read from the input file */
+    	macro *macros;/* array of struct macro to hold all the macros found in the input file */
+    	int lineCount =0;/* counter for the current line number in the input file */
 
-	int countMcr = 0;
-	int inMcr = 0;
-	int errFlag = 0;
-	char *mcrPtr;    
+	int countMcr = 0;/* counter for the total number of macros found in the input file */
+	int inMcr = 0;/* flag to indicate if the current line is inside a macro definition or not */
+	char *mcrPtr;    /* pointer to point to the current position of the macro in the current line */
 
-	/* Reading line by line from input file */
+	/* Counting the total number of macros in the input file */
     	while (fgets(line, MAX_LINE, ipf) != NULL) 
-    		  if (strstr(line, "mcr") != NULL)/*count every time we find macros*/
+    		  if (strstr(line, "mcr") != NULL)
     		  	countMcr++;
     		  	
     	
-    	
+    	/* Rewind the input file pointer to the beginning of the file */
        	rewind(ipf);
     	       	
     	
-    	/* Creating an array of macros */
-    	macros = (struct macro*)(malloc(sizeof(struct macro)*(countMcr)));
-    	if(macros == NULL)
-        {
-       		free(macros);
-        	printf("Error: not enough space in memory\n");
-        	exit(0);
-        }
-
-   		/* Open file {filename.am} for output */
+    	/* Allocating memory for the array of macros */
+    	macros = (macro*)(malloc_with_check(sizeof(macro)*(countMcr)));
+    	
+   	/* Open file {filename.am} for output */
     	opf = fopen(fileName, "w");
     	if (opf == NULL) 
     	{
         	printf("Error: unable to open %s.am for writing.\n",fileName);
         	exit(0);
-		}
+	}
 
     	/* Reading line by line from input file */
     	while (fgets(line, MAX_LINE, ipf)) 
     	{
         	lineCount++;
-        	
-        	if ( strstr(line, "mcr") != NULL)/*found macro*/
-        	{	
-				inMcr = 1;
-            	/* Extract the name of the macro */
-            	if(sscanf(line, "mcr %s %s", macros[curr_macro].name,line)==2){
-					/* Check for extranious context after mcr name */
-					if(line != '\0'){
-						printf("Error in line %d: extranious context after mcr name\n",lineCount);
-						errFlag = 1;
-					}
-				}	
-   	
-      			
-            	/* Read the content of the macro into a buffer */
-            	while (inMcr==1) 
-            	{
-                	fgets(line, MAX_LINE, ipf);	
-					lineCount++;
 
-					if( (mcrPtr = strstr(line,"endmcr")) != NULL) /*found endmcr*/
+		/* If the current line contains the keyword "mcr", it is a macro definition */
+        	if ((mcrPtr = strstr(line, "mcr")) != NULL)
+        	{	
+			inMcr = 1;
+          	  	/* Extract the name of the macro */
+            		if(sscanf(mcrPtr, " mcr %s %s", macros[curr_macro].name,line)==2)
+            		{
+				/* Check for extranious context after mcr name */
+				if(line != '\0')
+				{
+					printf("Error in line %d: extranious context after mcr name\n",lineCount);
+				}
+			}	
+			/* Check for extranious context before mcr name */
+			for(--mcrPtr;mcrPtr>=line;mcrPtr--)
+			{
+				if(!isspace(*mcrPtr) && *mcrPtr!='\0')
+				{
+					printf("Error in line %d: extranious context befor mcr name\n",lineCount);
+					break;
+				}
+			}
+			/* Check if the macro name is a reserved word */
+   			printf("in line %d:mcr name:%s\n",lineCount, macros[curr_macro].name);
+      			if(is_reserved_word( macros[curr_macro].name))
+      			{
+				printf("Error in line %d: mcr %s is reserved word and cannot be mcr name\n",lineCount,macros[curr_macro].name);
+			}
+            		/* Read the content of the macro into a buffer */
+            		while (inMcr==1)
+            		{
+				if(fgets(line, MAX_LINE, ipf) == NULL)
+				{
+					printf("Error in line %d: reach EOF befor endmcr\n",lineCount);
+					return 0;
+				}	
+				lineCount++;
+
+				if( (mcrPtr = strstr(line,"endmcr")) != NULL) /*found endmcr*/
                 		{
-							inMcr=0;
+					inMcr=0;
                 			i = (int)(mcrPtr - line);/*get the index of endmcr*/
                 			line[i] = '\0';/*get rid of endmcr*/
                 			
                 			/*print what comes after endmcr*/
                 			mcrPtr += MCR_END_SIZE;
-                    		mcrPtr += skipSpacesAndTabs(mcrPtr);
-							/* Check for extranious context after endmcr */
-                		if(mcrPtr[0] != '\0'){
-							printf("Error in line %d: extranious context after endmcr\n",lineCount);
-							errFlag = 1;
-						}
-                	}
-                	/*macro too long*/
-                	if(strlen(macros[curr_macro].content) + strlen(line) > sizeof(macros[curr_macro].content) -1)
-                	{
-                     	printf("Error in line %d: macro %s content exceed max size\n",lineCount,macros[curr_macro].name);
-						errFlag = 1;
-                     	continue;
-                    }
+                    			mcrPtr += skipSpacesAndTabs(mcrPtr);
+                  
+					/* Check for extranious context after endmcr */
+                			if(mcrPtr[0] != '\0')
+                			{
+						printf("Error in line %d: extranious context after endmcr\n",lineCount);
+					}
+                		}
+                		/* Check if the macro content exceeds the maximum size */
+                		if(strlen(macros[curr_macro].content) + strlen(line) > sizeof(macros[curr_macro].content) -1)
+                		{
+                    	 		printf("Error in line %d: macro %s content exceed max size\n",lineCount,macros[curr_macro].name);
+                    		}
                     		
-                	strcat(macros[curr_macro].content, line);
+                		strcat(macros[curr_macro].content, line);
                 	}
 
             		curr_macro++;
         	}
-        	else
+        	else /* Current line is not a macro definition */
         	{
             		if(sscanf(line,"%s", word)==1)/*if able to read a word*/
             		{
@@ -118,7 +129,7 @@ bool preAssembler(FILE *ipf, char* fileName)
                     			if(!strcmp(word,macros[i].name))/*the word is a macro*/
                     			{
                         			replaced = 1;
-                        			fprintf(opf, "%s\n", macros[i].content);/*print the macro content*/
+                        			fprintf(opf, "%s", macros[i].content);/*print the macro content*/
                         
                         			break;
                     			}
@@ -129,11 +140,13 @@ bool preAssembler(FILE *ipf, char* fileName)
         	}
 	
     	}
-
+	/* Free the memory used by the array of macros */
     	free(macros);
+    	
+    	/* Close the output file */
     	fclose(opf);
-		if(errFlag == 1)
-			exit(0);
+    	
     	return 0;
 }
+
 
