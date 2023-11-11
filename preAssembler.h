4a2e6755229e04944db8dabd33a0c3c35bf8fb70
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2023-11-11 09:58:24 +0200

    Update preAssembler.h

--- preAssembler.h
+++ preAssembler.h
@@ -1,16 +1,35 @@
+/*
+This header file contains declarations for the pre-assembly process in the assembly language compiler. 
+It includes utils.h and declarations.h for necessary data structures and function prototypes.
+*/
+
 #ifndef _PREASSEMBLER_H
 #define _PREASSEMBLER_H
 
 #include "utils.h"
 #include "declarations.h"
 
-
+/*
+ * Function to perform pre-assembly process
+ *
+ * @param ipf A pointer to a FILE structure representing the input file
+ * @param fileName A character pointer to the name of the input file
+ *
+ * @returns A boolean value indicating whether the pre-assembly process was successful or not
+ */
 bool preAssembler(FILE *ipf, char* fileName);
 
+/*
+ * A struct to represent a macro in the pre-assembly process
+ *
+ * @param name A character array representing the name of the macro
+ * @param content A character array representing the content of the macro
+ */
 typedef struct macro {
     char name[MAX_MCR];
     char content[MAX_MCR];
 } macro;
 
-
 #endif
+
+
