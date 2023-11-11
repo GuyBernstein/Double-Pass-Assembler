01a8666966d9be416395e883cc29b8343057fa95
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2023-11-11 10:01:03 +0200

    Update table.c

--- table.c
+++ table.c
@@ -1,4 +1,4 @@
-/* Implements a basic table ("dictionary") data structure. sorted by value, ascending. */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,71 +6,204 @@
 #include "table.h"
 #include "utils.h"
 
-void add_table_item(table *tab, char *key, long value, symbol_type type) {
-	char *temp_key;
-	table prev_entry, curr_entry, new_entry;
-	/* allocate memory for new entry */
-	new_entry = (table) malloc_with_check(sizeof(table_entry));
-	/* Prevent "Aliasing" of pointers. Don't worry-when we free the list, we also free these allocated char ptrs */
+/* This function adds an item to a table. */
+/* It takes in the table head, a key, a value, and a symbol type as parameters. */
+void add_table_item(table *head, char *key, short value, symbol_type type) 
+{
+	/* Declare local variables */
+	char *temp_key; /* Temporary variable to hold the key */
+	table prev_node, curr_node, new_node; /* Pointers to previous, current, and new nodes */
+
+	/* Allocate memory for the new node */
+	new_node = (table) malloc_with_check(sizeof(table_node));
+
+	/* Allocate memory for the temporary key */
 	temp_key = (char *) malloc_with_check(strlen(key) + 1);
+
+	/* Copy the key into the temporary variable */
 	strcpy(temp_key, key);
-	new_entry->key = temp_key;
-	new_entry->value = value;
-	new_entry->type = type;
-	/* if the table's null, set the new entry as the head. */
-	if ((*tab) == NULL || (*tab)->value > value) {
-		new_entry->next = (*tab);
-		(*tab) = new_entry;
+
+	/* Assign the values to the new node */
+	new_node->key = temp_key;
+	new_node->value = value;
+	new_node->type = type;
+
+	/* If the table is null or the new node's value is less than the head's value, set the new node as the head */
+	if ((*head) == NULL || (*head)->value > value) 
+	{
+		new_node->next = (*head);
+		(*head) = new_node;
 		return;
 	}
 
-	/* Insert the new table entry, keeping it sorted */
-	curr_entry = (*tab)->next;
-	prev_entry = *tab;
-	while (curr_entry != NULL && curr_entry->value < value) {
-		prev_entry = curr_entry;
-		curr_entry = curr_entry->next;
-	}
+	/* Insert the new table node, keeping it sorted */
+	curr_node = (*head)->next;
+	prev_node = *head;
 
-	new_entry->next = curr_entry;
-	prev_entry->next = new_entry;
+	while (curr_node != NULL && curr_node->value < value) 
+	{
+		prev_node = curr_node;
+		curr_node = curr_node->next;
+	}
+	/*set the next pointer to not lose the list*/
+	new_node->next = curr_node;
+	prev_node->next = new_node;
 }
 
-void free_table(table tab) {
-	table prev_entry, curr_entry = tab;
-	while (curr_entry != NULL) {
-		prev_entry = curr_entry;
-		curr_entry = curr_entry->next;
-		free(prev_entry->key); /* Didn't forget you!ssss */
-		free(prev_entry);
+/* This function frees the memory allocated for a table. */
+/* It takes in the table head as a parameter. */
+void free_table(table head) 
+{
+	/* Declare local variables */
+	table prev_node, curr_node = head;
+
+	/* Traverse the list and free each node */
+	while (curr_node != NULL) 
+	{
+		prev_node = curr_node;
+		curr_node = curr_node->next;
+
+		/* Free the memory allocated for the key */
+		free(prev_node->key);
+
+		/* Free the memory allocated for the node */
+		free(prev_node);
 	}
 }
-table_entry *find_by_types(table tab, char *key, int symbol_count, ...) {
+
+    
+/* This function adds a value to a specific symbol type in a table. */
+/* It takes in the table head, the value to add, and the symbol type as parameters. */
+void add_value_to_type(table head, short to_add, symbol_type type) 
+{
+	/* Declare a table(the list) */
+	table curr_node;
+
+	/* Traverse the list and add the value to each node with the same symbol type */
+	for (curr_node = head; curr_node != NULL; curr_node = curr_node->next)
+	{
+		if (curr_node->type == type) 
+			curr_node->value += to_add;
+	}	
+}
+
+
+/* This function finds a node in a table by a given key and list of valid symbol types. 
+ * It takes in the table head, the key to search for, the number of valid symbol types, 
+ * and a variable list of symbol types as parameters. */
+table_node *find_by_types(table head, char *key, int symbol_count, ...) 
+{
 	int i;
+	/* Allocate memory for an array of valid symbol types */
 	symbol_type *valid_symbol_types = malloc_with_check((symbol_count) * sizeof(int));
-	/* Build a list of the valid types */
+
+	/* Build the list of valid symbol types from the variable argument list */
 	va_list arglist;
 	va_start(arglist, symbol_count);
-	for (i = 0; i < symbol_count; i++) {
+	for (i = 0; i < symbol_count; i++) 
 		valid_symbol_types[i] = va_arg(arglist, symbol_type);
-	}
 	va_end(arglist);
-	/* table null => nothing to dos */
-	if (tab == NULL) {
+
+	/* If the table is empty, there is nothing to search, so return NULL */
+	if (head == NULL) 
+	{
 		free(valid_symbol_types);
 		return NULL;
 	}
-	/* iterate over table and then over array of valid. if type is valid and same key, return the entry. */
+
+	/* Iterate over the table and the array of valid symbol types */
 	do {
-		for (i = 0; i < symbol_count; i++) {
-			if (valid_symbol_types[i] == tab->type && strcmp(key, tab->key) == 0) {
+		for (i = 0; i < symbol_count; i++) 
+		{
+			/* If the symbol type matches and the key matches, return the node */
+			if (valid_symbol_types[i] == head->type && strcmp(key, head->key) == 0) 
+			{
 				free(valid_symbol_types);
-				return tab;
+				return head;
 			}
 		}
-	} while ((tab = tab->next) != NULL);
-	/* not found, return NULL */
+	} while ((head = head->next) != NULL);
+
+	/* If the node is not found, free the memory allocated for the valid symbol types and return NULL */
 	free(valid_symbol_types);
 	return NULL;
 }
+
+/*
+ * Create an output file for a given symbol type containing all nodes of that type.
+ * If no nodes of the given type exist, no file is created.
+ * Return false only if cant open file(because then needs to write a message and coninue the program in assemble.c)
+ */
+bool create_output_file(table_node* head, symbol_type type, char *file_name) 
+{
+	char* fileName;
+	FILE* fp;
+	table_node* current = head;
+	
+	/* Check if there are any nodes with the desired type */
+    	while (current != NULL) 
+    	{
+        	if (current->type == type) 
+        	{
+            		break;
+        	}
+        	current = current->next;
+    	}
+    	if (current == NULL) 
+    	{
+        /* No nodes with the desired type, do not create output file */
+        	return TRUE;
+    	}
+    	
+	/* Determine the output file name based on the symbol type */
+  	switch (type) 
+  	{
+  		case EXTERNAL_REFERENCE:
+			fileName = strallocat(file_name, ".ext");
+            		break;
+        	case ENTRY_SYMBOL:
+            		fileName = strallocat(file_name, ".ent");
+            		break;
+        	default:
+            		/* Invalid symbol type, do nothing */
+            		return TRUE;
+    	}
+	
+	
+    	/* Open the output file for writing */
+    	fp = fopen(fileName, "w");
+    	if (fp == NULL) 
+    	{
+		free(fileName);
+        	return FALSE;
+    	}
+    	
+
+    	/* Traverse the list and write matching nodes to the output file */
+    	current = head;
+    	while (current != NULL) 
+    	{
+        	if (current->type == type) 
+        	{
+            		fprintf(fp, "%s %d\n", current->key, current->value);
+        	}
+        	current = current->next;
+    	}
+
+    	/* Close the output file and free filename*/
+    	free(fileName);
+    	fclose(fp);
+    	return TRUE;
+}
+
+
+
+    
+
+
+
+
+
+
+
 
