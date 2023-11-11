b3d3c12a20fb47b11e401877d5eb720195666ad8
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2023-11-11 10:02:32 +0200

    Update table.h

--- table.h
+++ table.h
@@ -1,47 +1,77 @@
 /* Implements a dynamically-allocated symbol table */
+
 #ifndef _TABLE_H
 #define _TABLE_H
 
+#include "declarations.h"
+
 /** A symbol type */
 typedef enum symbol_type {
-	CODE_SYMBOL,
-	DATA_SYMBOL,
-	EXTERNAL_SYMBOL,
-	/** Address that contains a reference to (a usage of) external symbol */
-	EXTERNAL_REFERENCE,
-	ENTRY_SYMBOL
+    CODE_SYMBOL,        /* Symbol contains code */
+    DATA_SYMBOL,        /* Symbol contains data */
+    EXTERNAL_SYMBOL,    /* Symbol is external */
+    EXTERNAL_REFERENCE, /* Symbol is a reference to an external symbol */
+    ENTRY_SYMBOL        /* Symbol is an entry point */
 } symbol_type;
 
-/** pointer to table entry is just a table. */
-typedef struct entry* table;
+/** pointer to table node is just a table. */
+typedef struct node* table;
 
-/** A single table entry */
-typedef struct entry {
-	/** Next entry in table */
-	table next;
-	/** Address of the symbol */
-	long value;
-	/** Key (symbol name) is a string (aka char*) */
-	char *key;
-	/** Symbol type */
-	symbol_type type;
-} table_entry;
+/** A single table node */
+typedef struct node 
+{
+    table next;     /* Next node in table */
+    short value;    /* Address of the symbol */
+    char *key;      /* Key (symbol name) is a string (aka char*) */
+    symbol_type type;   /* Symbol type */
+} table_node;
 
 /**
- * Adds an item to the table, keeping it sorted.
+ * Print the contents of the symbol table.
+ * @param tab Pointer to the symbol table.
  */
-void add_table_item(table *tab, char *key, long value, symbol_type type);
+void print_table(table tab);
 
 /**
- * free all the memory required by the table.
+ * Add an item to the symbol table.
+ * @param tab Pointer to the symbol table.
+ * @param key The key (symbol name) for the new item.
+ * @param value The value (address) for the new item.
+ * @param type The type of the new item.
+ */
+void add_table_item(table *tab, char *key, short value, symbol_type type);
+
+/**
+ * Free all the memory used by the symbol table.
+ * @param tab Pointer to the symbol table.
  */
 void free_table(table tab);
 
 /**
- * Adds the value to add into the value of each entry
+ * Find a node in the symbol table by key and type.
+ * @param tab Pointer to the symbol table.
+ * @param key The key (symbol name) to search for.
+ * @param symbol_count The number of symbol types to search for.
+ * @param ... A variable number of arguments of type symbol_type to search for.
+ * @return Pointer to the found node or NULL if not found.
  */
+table_node *find_by_types(table tab,char *key, int symbol_count, ...);
 
+/**
+ * Add a value to the value field of each node of a specific symbol type.
+ * @param tab Pointer to the symbol table.
+ * @param to_add The value to add to each node of the specified type.
+ * @param type The symbol type to modify.
+ */
+void add_value_to_type(table tab, short to_add, symbol_type type);
 
-table_entry *find_by_types(table tab,char *key, int symbol_count, ...);
+/**
+ * Create an output file with a list of symbols of a specific type.
+ * @param head Pointer to the first node in the symbol table.
+ * @param type The type of symbols to include in the output file.
+ * @param file_name The name of the output file to create.
+ * @return True if the output file was successfully created, false otherwise.
+ */
+bool create_output_file(table_node* head, symbol_type type, char *file_name);
 
 #endif
