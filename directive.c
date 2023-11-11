d87519190a9c31463af19b6154958ca5fe5ef94b
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2023-11-11 09:50:27 +0200

    ,directive.c

--- directive.c
+++ directive.c
@@ -1,187 +1,178 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <limits.h>
+#ifndef _DECLERATIONS_H
+#define _DECLERATIONS_H
+
+/* We print to stderr for errors*/
+#define ERR_OUTPUT_FILE stderr
+
+/* Maximum length of a single source line */
+#define MAX_LINE 80
+
+/* Maximum number of macros allowed */
+#define MAX_MCR 256
+
+/* Size of the "endmcr" string */
+#define MCR_END_SIZE 6
+
+/* Maximum length of a label allowed */
+#define MAX_LABLE_LEN 30
+
+/* Initializing IC to be 100 */
+#define IC_START_VALUE 100
+
+/* Total number of memory cells */
+#define MEMORYSIZE 256
+
+/* Total number of directives that we use in assembly */
+#define NUM_OF_DIRECTIVES 4
+
+/* Total number of functions that we use in assembly */
+#define NUM_OF_FUNCS 16
+
+/* The range between 14 bits of numbers */
+#define SHRT_MIN_14_BITS -8192
+#define SHRT_MAX_14_BITS 8192
+
+/* The maximum number of operands for the functions in assembly that we use */
+#define MAX_TOKENS 2
+
+/* for function use to take note if its second or first group */
+#define FIRSTGROUPTARGET 0
+#define FIRSTGROUPSOURCE 1
+#define SECONDGROUP 2
+
+/*
+ * Macro that moves the given string pointer to the next non-whitespace character
+ */
+#define MOVE_TO_NOT_WHITE(string, index) \
+        for (;string[(index)] && (string[(index)] == '\t' || string[(index)] == ' '); (++(index)))\
+        	;
+
+/* Macro for freeing memory allocated for files */
+#define FREE_FILES(file1,file2,file3) \
+	free(file1); \
+	free(file2); \
+	free(file3);
+
+/* Boolean type used in the program */
+typedef enum booleans {
+	FALSE = 0, 
+	TRUE = 1
+} bool;
+
+/* Struct that keeps the information of the line in the file */
+typedef struct line_info {
+	/* Line number in file */
+	short line_number;
+	/* File name */
+	char *file_name;
+	/* Line content (source) */
+	char *content;
+} line_info;
+
+/*
+	This union represents a line of code, including any associated label and binary code.
+	It will be used in step two of the code execution process.
+*/
+typedef union line_code {
+
+	short code; /* A short integer representing the binary code associated with this line of code.*/
+
+	struct {
+    		line_info line; /* Represents a line of code.*/
+    		char label[MAX_LABLE_LEN]; /* Represents a label associated with a line of code.*/
+    		short code; /* A short integer representing the binary code associated with this line of code.*/
+	} code_info;
+} line_code;
+
+/* Enumeration for directive types (.data, .entry, etc.) */
+typedef enum directive {
+	DATA_DIRE,      /* .data directive */
+	EXTERN_DIRE,    /* .extern directive */
+	ENTRY_DIRE,     /* .entry directive */
+	STRING_DIRE,    /* .string directive */
+	NONE_DIRE,      /* Not found */
+	ERROR_DIRE      /* Parsing/syntax error */
+} directive;
+
+/* Enumeration for command opcodes, with NONE_OP for not found */
+typedef enum opcodes {
+	MOV_OP,
+	CMP_OP,
+	ADD_OP,
+	SUB_OP,
+	NOT_OP,
+	CLR_OP,
+	LEA_OP,
+	INC_OP,
+   	DEC_OP,
+	JMP_OP,
+	BNE_OP,
+	RED_OP,
+  	PRN_OP,
+	JSR_OP,
+	RTS_OP,
+	STOP_OP,
+
+	/* Failed/Error */
+	NONE_OP = -1
+} opcode;
+
+/* Enumeration for registers - r0->r7 + not found */
+typedef enum registers {
+	R0,
+	R1,
+	R2,
+	R3,
+	R4,
+	R5,
+	R6,
+	R7,
+	NONE_REG = -1
+} reg;
 
 
-#include "utils.h"
 
-
-
-/* Returns the first directive from the specified index. if no such one, returns NONE */
-directive find_directive_from_index(line_info line, int *index)
+/** Operand addressing type */
+typedef enum addressing_types 
 {
-	char temp[MAX_LINE];
-	
-	int j;
-	directive result;
+	/** Immediate addressing (0) */
+	IMMEDIATE_ADDR ,
+	/** Direct addressing (1) */
+	DIRECT_ADDR ,
+	/** Relative addressing (2) */
+	RELATIVE_ADDR ,
+	/** Register addressing (3)*/
+	REGISTER_ADDR ,
+	/** Failed/Not found that its an addressing */
+	NONE_ADDR = -1
+} addressing_type;
 
-	MOVE_TO_NOT_WHITE(line.content, *index) /* get index to first not white place */
-	if (line.content[*index] != '.') 
-		return NONE_DIRE;
-	
-	/* Let's allocate some memory to the string needed to be checked */
-	for (j = 0; line.content[*index] && line.content[*index] != '\t' && line.content[*index] != ' '; (*index)++, j++) 
-		temp[j] = line.content[*index];
-	
-	temp[j] = '\0'; /* End of string */
-	
-	/* if invalid instruction but starts with '.', return error */
-	if ((result = find_directive_by_name(temp+1)) != NONE_DIRE) 
-		return result;
-		
-	printf_line_error(line, "Invalid instruction name: %s", temp);
-	return ERROR_DIRE; /* starts with '.' but not a valid zz! */
-}
-
-
-/* This function recieves an integer of the line number and a string
- * returns a string(or NULL if invalid string)*/
-int checkValidString(line_info line, char *string,char *str)
+/** Operand encoding type */
+typedef enum encoding_types 
 {
-	
-	char *ptrString = string;/*pointer to the given string*/
-	
-	/* Check if the string is empty */
-	if (string == NULL) 
-	{
-		printf_line_error(line,"(%s).string must recive input\n",ptrString);
-		return 0;
-	}
-	ptrString += skipSpacesAndTabs(ptrString);/*skip the first spaces*/
-	
-	
-	
-	if(ptrString[0] != '"')/*check for quotation marks at the beggining of the string*/
-	{
-		printf_line_error(line,"(%s) need quotation marks at the beggining of the string\n",ptrString);
-		return 0;
-	}
-	
-	ptrString += 1;/*advance the pointer after the first quotation mark*/
-	strcpy(str,ptrString);/*copy the string for output*/
-
-	if ((ptrString = strchr(str,'"')) == NULL)/* search for another quotation mark 
-							 * if cant find, print a message
-							 ptrString points to the second quotation marks at str*/
-	{
-		printf_line_error(line,"(%s) need quotation marks at the ending of the string\n",ptrString);
-		return 0;
-	}
-	ptrString[0]= '\0';/*change the second quotation mark to \0 */
-
-	ptrString += 1;/*advance the pointer after the second quotation mark*/
-  	ptrString += skipSpacesAndTabs(ptrString);/*skip spaces after quotation mark*/
-
-	if(ptrString[0]=='\0'){/*found nothing's after the second quotation mark*/
-
-		return 1;/*return the string*/
-	}
-	else/*found something after the second quotation mark*/
-	{
-
-		printf_line_error(line," extranious text after ending quotation marks of the string\n");
-		return 0;
-	}	
-
-		
-	
-}
+	/** Absolute encoding (0) */
+	ABSOLUTE,
+	/** External encoding (1) */
+	EXTERNAL,
+	/** Relocatable encoding (2) */
+	RELOCATEBLE
 
 
-/* This function recieves an integer of the line number and a string
- * returns an array of integers(or NULL if invalid data)*/
-int checkValidData(line_info line, char *data,short* numbers)
-{
+} encoding_types;
 
-	
-	char *token;/* a pointer the numbers in data*/
-	char *endptr;/*a pointer to what comes after the numbers*/
-	long number;/*the number*/
-	int num_numbers = 0;/*index of the numbers in the data*/
-	char *ptrData = data;/*pointer to the data string*/
-	int dataLen;/*the length of the data*/
-	
-	/* Check if the data is empty */
-	if (data == NULL) 
-	{
-		printf_line_error(line, ".data must recive input");
-		return 0;
-	}
-	
-	removeSpaces(ptrData);/*remove all spaces*/
-	
-	dataLen = strlen(ptrData);/*the length of the data string(without spaces)*/
-	
-	
-	
-	
-	
 
-	/* Check if the input string starts or ends with commas*/
-	if (ptrData[0] == ',' || ptrData[dataLen-1] == ',') 
-	{
-		printf_line_error(line,"(%s) illegal comma.\n",ptrData);
-		return 0;
-	}
-  
-	/* Check if the data has consecutive commas*/
-	if (strstr(data, ",,") != 0) 
-	{
-		printf_line_error(line,"(%s) consecutive commas.\n",ptrData);
-		return 0;
-	}
+#endif
 
-	/* Get the first number */
-	token = strtok(ptrData, ",");
-	while (token != 0)
-	{
-	
-	
-		/* Check if the token is an integer */
-		number = strtol(token, &endptr, 10);
-		if (*endptr != '\0') 
-		{
-			
-			printf_line_error(line,"(%s) not an integer\n",endptr);
-			return 0;
-		}
-		/* Check if the number is short  */
-   		if (number < SHRT_MIN || number > SHRT_MAX ) 
-   		{
-			printf_line_error(line,"%ld value out of range\n",number);
-     			return 0;
-    		}
-		/* Add the number to the array */
-		numbers[num_numbers++] = (short)number;
-		/* Get the next token */
-		token = strtok(NULL, ",");
-	}
-	return 1;/*return the array*/
-}
 
-/* Adding an input short to the data array 
-	also counts the amount of numbers and add to dc counter */
-bool insertDataToArray(short* input, short* output,int* dc) {
-	int i;
-	for(i=0;input[i]!='\0';i++,(*dc)++){
-		output[*dc] = input[i];
-    printf("%d: %d\n",*dc,output[*dc]);	/* to be removed */
-	} 
-	return TRUE;
-}
 
-/* Adding an input String to the data array and converting it to short
-	 also counts the chars and add to dc counter */
-bool insertStringToArray(char* input, short* output,int* dc) {
-	int i;
-	for(i=0;input[i]!='\0';i++,(*dc)++){
-		output[*dc] = (short)input[i];
-    printf("%d: %d\n",*dc,output[*dc]);	/* to be removed */
-	}
-	output[*dc] = '\0';	/* add '\0' at the end */
-	printf("%d: %d\n",*dc,output[*dc]); /* to be removed */	
-	(*dc)++;
-	return TRUE;	 
-}
+
+
+
+
+
+
+
+
+
+
+
